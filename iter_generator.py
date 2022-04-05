b4f8e2c7a8b7014ad59c2ba91eec41092aff58fb
TaoJing96 <2205674160@qq.com>
2022-04-05 14:22:26 +0800

    ADD: 生成器demo

--- iter_generator.py
+++ iter_generator.py
@@ -2,6 +2,7 @@ class NodeIter:
     def __init__(self, node):
         self.node = node
 
+    # 迭代对象
     def __next__(self):
         if self.node is None:
             raise StopIteration
@@ -14,8 +15,24 @@ class Node:
         self.name = name
         self.next = None
 
+    # 返回迭代器
     def __iter__(self):
         return NodeIter(self)
+
+
+# yield生成器
+def fibonacci(n):
+    a, b, num = 1, 1, 1
+    while True:
+        if num > n:
+            return
+        yield a
+        a, b, num = b, a + b, num + 1
+
+
+def generator_demo():
+    for i in fibonacci(5):
+        print(i)
 
 
 if __name__ == '__main__':
@@ -25,3 +42,5 @@ if __name__ == '__main__':
     print(dir(n1))
     for n in n1:
         print(n.name)
+    print("generator_demo")
+    generator_demo()
